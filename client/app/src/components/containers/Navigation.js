23a877a015aa311e923b3bc67dc749bcc19d1361
ivvlko <ivankoevbg@gmail.com>
2022-11-14 20:36:50 +0200

    add basic styling

--- client/app/src/components/containers/Navigation.js
+++ client/app/src/components/containers/Navigation.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 function Navigation(){
     return (
-        <nav>
+        <nav className="navMenu">
             <Link to="">Home</Link>
             <Link to="play">Play</Link>
-            <Link to="login">Login</Link>
+            <Link className="loginButton" to="login">Login</Link>
         </nav>
     )
 }
