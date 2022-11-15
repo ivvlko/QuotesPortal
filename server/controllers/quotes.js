979783fadc67395ede482057bf1d508574e50738
ivvlko <ivankoevbg@gmail.com>
2022-11-15 20:51:09 +0200

    setup question endpoint

--- server/controllers/quotes.js
+++ server/controllers/quotes.js
@@ -1,11 +1,11 @@
-import { pseudoModelQuoteelQuote } from "../utils/pseudoModelQuoteel";
+const pseudoModelQuote = require("../utils/pseudoModel");
 
 const randomQuoteController = async (req, res) => {
 
     try {
-        let totalCount = await pseudoModelQuoteel.countDocuments({}).exec();
+        let totalCount = await pseudoModelQuote.pseudoModelQuote.countDocuments({}).exec();
         randomInt = Math.floor(Math.random() * totalCount);
-        quote = await pseudoModelQuoteel.findOne({}).skip(randomInt);
+        quote = await pseudoModelQuote.pseudoModelQuote.findOne({}).skip(randomInt);
         res.status(200).json(quote);
     }
     catch (e) {
@@ -18,7 +18,7 @@ const filteredQuotesController = async (req, res) => {
     try{
         const {author, quote, category} = req.query;
 
-        filteredQuotes = await pseudoModelQuoteel.find({
+        filteredQuotes = await pseudoModelQuote.pseudoModelQuote.find({
             "author": {"$regex": author ? author : ''},
             "quote": {"$regex": quote ? quote : ''},
             "category": {"$regex": category ? category : ''},
