50307955629199f1d74a4f84f240ae175f31ea66
ivvlko <ivankoevbg@gmail.com>
2022-11-19 15:09:53 +0200

    game initial stage first question

--- client/app/src/components/details/Game.js
+++ client/app/src/components/details/Game.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { getEasyQuestion } from '../../services/genericQueries';
+
 export default function Game() {
+
+    const [currentQuestion, setCurrentQuestion] = useState(null);
+
+    useEffect(() => {
+        async function getQuestion() {
+            const response = await getEasyQuestion();
+            setCurrentQuestion(() => response);
+        }
+        getQuestion();
+        
+    }, [])
+
     return (
         <div>
-            <h1>Play</h1>
+            <h1>{ currentQuestion ? currentQuestion.quote : "loading.." }</h1>
         </div>
     )
 }
--- client/app/src/services/genericQueries.js
+++ client/app/src/services/genericQueries.js
@@ -5,3 +5,8 @@ export async function getRandomQuote(){
     const response = await axios.get("http://localhost:8000/quotes/random-quote");
     return await response.data;
 }
+
+export async function getEasyQuestion(){
+    const response = await axios.get("http://localhost:8000/questions/easy-question");
+    return await response.data;
+}
